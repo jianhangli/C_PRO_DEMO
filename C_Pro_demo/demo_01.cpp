4fbe48db48cb532f9a33a99c1239bd1c18801c27
jianhangli <jianhangli123@163.com>
2021-10-19 21:59:54 +0800

    2021年10月19日 Signed-off-by: jianhangli <jianhangli123@163.com>

---
+++ C_Pro_demo/demo_01.cpp
@@ -0,0 +1,35 @@
+#if 0
+/*
+说明，将一个8位的2进制数转化为10进制的数
+如：输入 01101001
+	输出 105
+
+*/
+
+#include<iostream>
+using namespace std;
+double power(double x, int n);		//定义指数函数
+int main() {
+	int value = 0;
+	cout << "输入8进制";
+	for (int i = 7; i >= 0; i--)
+	{
+		char ch;
+		cin >> ch;
+		if (ch == '1')
+		{
+			value += static_cast<int>(power(2, i));
+		}
+	}
+	cout << "转换后的值为" << value << endl;
+	return 0;
+}
+double power(double x, int n) {
+	double val = 1.0;
+	while (n--)
+	{
+		val *= x;
+	}
+	return val;
+}
+#endif 
---
+++ README.md
@@ -0,0 +1,10 @@
+项目：C_PRO_DEMO
+
+说明：用于存放C++语言部分案例
+
+### C_Pro_demo
+
+说明：用于存放B站视频中的部分案例，网址为https://www.bilibili.com/video/BV1n7411278X?p=34&spm_id_from=pageDriver
+
+1. demo_01.cpp （说明，8位2进制进行10进制的转换）
+
